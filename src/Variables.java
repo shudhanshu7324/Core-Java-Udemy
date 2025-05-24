6997663038f2c1c2bad13405c673af1a27bafe85
Shudhanshu Shree <shudhanshu7324@gmail.com>
2025-05-24 18:05:08 +0530

    variables and primitive datatypes in java

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a+b);
+
+        // primitive datatypes
+        int x = 1; // 4 byte
+        float y = 45.67f; // 4byte
+        double d = 332.2323; // 8 byte
+        long l = 324243224l; // 8 bye
+
+        char c = 'c'; // 2 byte
+        boolean t = true; // 1 byte
+    }
+}
